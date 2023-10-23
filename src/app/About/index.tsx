a9b1ab2ef79a1a6901b16e58e644c1525378caa6
EvertonDaCunha <evertonldc21@gmail.com>
2023-10-23 18:47:34 -0300

    Alteração do texto sobre nós

--- src/app/About/index.tsx
+++ src/app/About/index.tsx
@@ -1,9 +1,13 @@
 export function About({id}:{id: string}) {
   return (
-    <section id={id} className="flex w-full flex-col items-start justify-center bg-slate-100 rounded-[50px] bg-cover sm:mt-20 h-[500px] bg-[url('/images/image-about.png')] bg-no-repeat bg-bottom bg-fixed">
-      <div className=" sm:w-[60%] flex flex-col items-start px-8 sm:ml-20 gap-4 ">
+    <section id={id} className="flex w-full flex-col items-start justify-center bg-slate-100 rounded-[50px] bg-cover sm:mt-20 bg-[url('/images/image-about.png')] bg-no-repeat bg-bottom bg-fixed">
+      <div className=" sm:w-[60%] flex flex-col items-start p-8 sm:ml-20 gap-4 ">
         <h2 className="text-4xl" >Sobre nós</h2>
-        <p className="text-lg text-start text-white font-light sm:w-[50%]">Somos um salão de beleza, com serviços de manicure, Pedicure, design de. sobrancelhas, henna, lavagem e hidratação de cabelos, aplicação de piercings.</p>
+        <p className="text-lg text-start text-white font-light sm:w-[50%]">Somos a Office Beauty, um Estúdio de Beleza e Tatuagem localizado no bairro Capão Raso em Curitiba, mais especificamente, no Edifício H. A. Offices Linha Verde. Um local de fácil acesso, onde temos um espaço preparado para recebê-la!</p>
+
+        <p className="text-lg text-start text-white font-light sm:w-[50%]">Aqui, oferecemos uma ampla variedade de serviços, incluindo manicure, pedicure, design de sobrancelhas, henna, mechas, tintura, escova, lavagem e hidratação de cabelos, aplicação de piercings, tatuagens e spa.</p>
+
+        <p className="text-lg text-start text-white font-light sm:w-[50%]">Temos como prioridade a excelência no que fazemos e no tratamento com nossos clientes, proporcionando um ambiente leve e acolhedor que eleva a autoestima e realça a sua beleza em cada detalhe.</p>
       </div>
     </section>
   )
--- src/app/OfficeBeauty/index.tsx
+++ src/app/OfficeBeauty/index.tsx
@@ -5,11 +5,11 @@ export function OfficeBeauty() {
       <>
         <div className="w-full h-[50vh] sm:h-[95vh] flex flex-col-reverse rounded-b-[200px] sm:rounded-b-[88px] top-0 sm:flex-row sm:justify-between bg-[url('/images/m-capa.png')] sm:bg-[url('/images/capa-1200.png')] sm:bg-cover bg-no-repeat">
         <div className="hidden sm:block m-auto">
-            <h1 className='left-0 flex text-5xl text-left font-light sm:w-[50%] text-[#fff]'>Oficce Beauty: A essência do cuidado feminino, refletindo sua beleza única.</h1>
+            <h1 className='left-0 flex text-5xl text-left font-light sm:w-[50%] text-[#fff]'>Office Beauty: A essência do cuidado feminino, refletindo sua beleza única.</h1>
           </div>
         </div>
           <div className="sm:hidden m-auto">
-            <p className='flex text-xl text-center font-light px-8 text-[#404040]'>A essência do cuidado feminino, refletindo sua beleza única.</p>
+            <h1 className='flex text-xl text-center font-light p-8 text-[#404040]'>Office Beauty: A essência do cuidado feminino, refletindo sua beleza única.</h1>
           </div>
       </>
     )
